f39b346c23102a6c390ab5458b0470ff0f6e5c2a
Yaroslav Kai <yaroslav.kai.mitpa.2025@lpnu.ua>
2026-06-01 12:17:53 +0000

    k

--- notify.sh
+++ notify.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 notify() {
-  local title="$1" msg="$2"
+  local title="$1" text="$2" level="${3:-info}"
   curl -s -o /dev/null -X POST \
-    "https://notify.events/api/v1/channel/ct-q5dageamlhvfjlsdlvlanmkxqwcfx/message" \
-    -H "Content-Type: application/json" \
-    -d "{\"title\":\"$title\",\"text\":\"$msg\"}"
+    "https://notify.events/api/v1/channel/source/e6qrxvp5q8bn_7-5luupdj_ao9wosckq/execute" \
+    -F "title=$title" \
+    -F "text=$text" \
+    -F "level=$level"
 }
